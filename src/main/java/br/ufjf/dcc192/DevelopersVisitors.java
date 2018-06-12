c80ff08042beb4ebfc25a7e22b3eefe73e438140
mateusgon <mateusgon57@gmail.com>
2018-06-12 17:23:46 -0300

    Adicionando mais telas ao código

--- src/main/java/br/ufjf/dcc192/DevelopersVisitors.java
+++ src/main/java/br/ufjf/dcc192/DevelopersVisitors.java
@@ -12,5 +12,7 @@ public class DevelopersVisitors implements CommitVisitor {
 	public void process(SCMRepository repo, Commit commit, PersistenceMechanism writer) {
                Commitss co = new Commitss(commit.getHash(), commit.getCommitter().getName(), commit.getCommitter().getEmail(), commit.getMsg(), commit.getModifications());
                Inicial.commits.add(co);
+               commit.getAuthor();
+               commit.getCommitter();
         }        
 }
